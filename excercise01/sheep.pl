f859bd638092ac69729e8741cbf2399490776f64
Thiemo Urselmann <thiemo.urselmann@hhu.de>
2017-10-12 18:38:09 +0200

    added predicates sheep/1, father/2, mother/2

--- excercise01/sheep.pl
+++ excercise01/sheep.pl
@@ -2,25 +2,12 @@
  * create sheep database
  */
 
-/* entities */
-
-sheep(hannes).
-sheep(gunter).
-sheep(willi).
-sheep(theo).
-
-sheep(maria).
-sheep(trude).
-sheep(klara).
-
-
 /* gender */
 
 male(hannes).
 male(gunter).
 male(willi).
 male(theo).
-
 female(maria).
 female(trude).
 female(klara).
@@ -38,14 +25,20 @@ parent(klara, maria).
  * define predicates
  */
 
-/* write predicate which is true for all sheeps */
+/* write a predicate sheep/1 which is true for all sheeps in this database */
+sheep(X) :- male(X); female(X).
 
+/**
+ * write two predicates father/2 and mother/2 with their
+ * first argument for the parent and the second for the child
+ */
+father(F, C) :-  male(F), parent(F, C).
+mother(M, C) :- female(M), parent(M, C).
 
-
-
-
-
-
+/**
+ * write a predicate ancestor/2 with its first argument for the
+ * ancestor and its second for its descendant
+ */
 
 
 
