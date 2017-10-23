51a2020ace871a1b5adc098cfbc063c6069c19c7
Thiemo Urselmann <thiemo.urselmann@hhu.de>
2017-10-23 14:41:47 +0200

    added del_element-pred

--- excercise02/list_predicates.pl
+++ excercise02/list_predicates.pl
@@ -6,3 +6,11 @@ is_a_list([_|T]) :- is_a_list(T).
 /* (c) predicate for testing if S is suffix of given list */
 suffix(S,S).
 suffix(S, [_|T]) :-  suffix(S, T).
+
+/* (e) checks wether E is element of L or not */
+element_of(E, [E|_]).
+element_of(E, [_|T]) :- element_of(E, T).
+
+/* (f) removes E from L and returns the new list R */
+del_element(E,[E|T],T).
+del_element(E,[S|T],[S|R]) :- del_element(E,T,R).
