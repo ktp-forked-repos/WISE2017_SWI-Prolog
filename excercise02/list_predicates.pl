5c62da40758605f237c1c951187469fabb6a2ca8
Thiemo Urselmann <thiemo.urselmann@hhu.de>
2017-10-23 14:11:31 +0200

    added suffix-pred

--- excercise02/list_predicates.pl
+++ excercise02/list_predicates.pl
@@ -1,3 +1,8 @@
-/* testing if L is list */
+/* (a) predicate for testing if L is list */
 is_a_list([]).
-is_a_list([_,T]) :- is_a_list(T).
+is_a_list([_|T]) :- is_a_list(T).
+
+
+/* (c) predicate for testing if S is suffix of given list */
+suffix(S,S).
+suffix(S, [_|T]) :-  suffix(S, T).
