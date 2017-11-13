e06902ee870efb390b7a42d1ec48e922a6c954fb
Thiemo Urselmann <thiemo.urselmann@hhu.de>
2017-11-13 19:13:02 +0100

    solved 4b,c

--- blatt3a4.pl
+++ blatt3a4.pl
@@ -1,4 +1,4 @@
-/* (a) gcd */
+/* (a) gcd/3 */
 
 gcd(L,G,Acc,R) :-
     0 is mod(G,Acc),
@@ -11,3 +11,18 @@ gcd(L,G,Acc,R) :-
 
 gcd(L,G,R) :-
     gcd(L,G,L,R).
+
+/* (b) coprime/2 */
+coprime(L,G) :-
+    gcd(L,G,R),
+    R is 1.
+
+/* (c) range/2 */
+range_1(0,Acc,Acc).
+range_1(M,Acc,L) :-
+    M > 0,
+    MNew is M - 1,
+    range_1(MNew,[M|Acc],L).
+
+range_1(M,L) :-
+    range_1(M,[],L).
